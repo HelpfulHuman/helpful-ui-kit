409c076f7af1d5d7a1a31a30c9449ac9d83b36ee
Ryan Canfield <ryancanfield@me.com>
2017-11-10 09:42:44 -0800

    Restructure for library use

---
+++ index.js
@@ -0,0 +1,16 @@
+import Table from "./Components/Table/Table";
+import TableRow from "./Components/Table/TableRow";
+import TableHeader from "./Components/Table/TableHeader";
+import TableBody from "./Components/Table/TableBody";
+import TableFooter from "./Components/Table/TableFooter";
+import TableHeaderColumn from "./Components/Table/TableHeaderColumn";
+import TableBodyColumn from "./Components/Table/TableBodyColumn";
+export {
+  Table,
+  TableRow,
+  TableHeader,
+  TableBody,
+  TableFooter,
+  TableHeaderColumn,
+  TableBodyColumn
+};
