48057e8774e8987cf4a423d5f38cbd9bc3b0d4ad
Travis Salad <travissalad@level11.com>
2017-12-18 12:49:25 -0800

    Added rect-dom to rollup config.

--- rollup.config.js
+++ rollup.config.js
@@ -8,7 +8,7 @@ export default {
         { file: "build/index.es.js", format: "es" },
     ],
     exports: "named",
-    external: ["react", "styled-components"],
+    external: ["react", "styled-components", "react-dom"],
     plugins: [
         resolve({ main: true }),
         typescript(),
